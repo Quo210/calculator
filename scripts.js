f40c0335cc720888ef720d7bbaaeddc3987346f8
Quo210 <gabrielrengifo211@gmail.com>
2021-07-22 23:38:11 -0400

    added basic math equations on the js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function sum(a,b){
+    return a + b
+}
+
+function substract(a,b) {
+    return a - b
+}
+
+function mult(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a / b
+}
+
+function sqrt(a) {
+    return  Math.floor(Math.sqrt(a) * 10) / 10
+}
+
+function percent(target,percent) {
+    return (target/100) * percent
+}
