48275db9b349fc6ee9b11890b6a6c640ee940111
Quo210 <gabrielrengifo211@gmail.com>
2021-07-26 20:15:18 -0400

    Created basic UI

--- scripts.js
+++ scripts.js
@@ -21,3 +21,46 @@ function sqrt(a) {
 function percent(target,percent) {
     return (target/100) * percent
 }
+
+function operate(numb1,numb2,operator) {
+    switch(operator) {
+        case (operator === '+'):
+            return sum(numb1,numb2)
+        break;
+        case (operator === '-'): 
+            return substract(numb1,numb2)
+            break;
+        case (operator === '*'):
+            return mult(numb1,numb2);
+            break;
+        case (operator === '/'):
+            return divide(numb1,numb2);
+            break;
+        case (operator === 'sq'):
+            return sqrt(numb1);
+            break;
+        case (operator === '%'):
+            return percent(numb1, numb2)
+            break;
+        default:
+            console.log(`${operator} is unknown.`)
+            return;
+    }  
+}
+
+const digitButtons = document.querySelectorAll('div#digits > button');
+const digitButtonsArray = Array.from(digitButtons);
+console.log(digitButtonsArray);
+let hue = 0;
+let saturation = 100;
+let value = 50;
+const paintDigits = () => {
+    hue = 10;
+    console.log(hue, saturation, value)
+    digitButtonsArray.forEach(element => {
+        element.style.backgroundColor = `hsl(${hue},${saturation}%,${value}%)`;
+        //console.log(`hsl(${hue},${saturation}%,${value}%)`)
+        saturation -= 25;
+        if (saturation < 35) { hue += 5; saturation = 100;}
+    })
+}
